ce9e94f7d596dede16863cd66f6964d11bc3b0c3
techllen <allenmwita21@gmail.com>
2023-12-02 14:04:42 -0500

    project 3

---
+++ Assignment/3/DBStudentApp_1174066/DataLayer/Repository.cs
@@ -0,0 +1,66 @@
+﻿using DBStudentApp_1174066.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DBStudentApp_1174066.DataLayer
+{
+    internal class Repository : IRepository //compose sql and send it to datalayer
+    {
+        IDataAccess _idac = new DataAccess();
+        //private DataTable dt;
+
+        public List<Course> GetAllCourses()
+        {
+            List<Course> CList = new List<Course>();
+            try
+            {
+                string sql = "select * from Courses";
+                DataTable dt = _idac.GetManyRowsCols(sql);
+                Console.WriteLine("Retrieved dt is  " + dt);
+                foreach(DataRow dr in dt.Rows)
+                {
+                    Course c = new Course();
+                    c.CourseNum = (string)dr["CourseNum"];
+                    c.CourseName = (string)dr["CourseName"];
+                    CList.Add(c);
+                }
+            }
+            catch(Exception ex){ throw; }
+            return CList;
+        }
+
+        public List<Enrollment> GetEnrollment(string courseNum)
+        {
+            List<Enrollment> eList = new List<Enrollment>();
+            try
+            {
+                string sql = "SELECT s.StudentId, s.FirstName, s.LastName, c.Credits " +
+                             "FROM Students s " +
+                             "JOIN Enrollments e ON s.StudentId = e.StudentId " +
+                             "JOIN Courses c ON e.CourseNum = c.CourseNum " +
+                             "WHERE c.CourseNum = '" + courseNum + "'"; ;
+
+                DataTable dt = _idac.GetManyRowsCols(sql); //datatable is mainly for repository class
+
+                foreach (DataRow dr in dt.Rows)
+                {
+                    Enrollment e = new Enrollment();
+                    e.StuedntId = (int)dr["StudentId"];
+                    e.FirstName = (string)dr["FirstName"];
+                    e.LastName = (string)dr["LastName"];
+                    e.Credits = (int)dr["Credits"];
+
+                    eList.Add(e);
+                }
+            }
+            catch (Exception ex) { throw; }
+            return eList;
+            
+        }
+    }
+}
---
+++ Assignment/3/DBStudentApp_1174066/Models/Enrollment.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DBStudentApp_1174066.Models
+{
+    internal class Enrollment
+    {
+        public int StuedntId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Credits { get; set; }
+    }
+}
