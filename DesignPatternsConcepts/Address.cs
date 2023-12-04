88838eac4635b1524b52cf32bb44f6b827bcdd80
techllen <allenmwita21@gmail.com>
2023-12-03 21:45:48 -0500

    project 5

---
+++ DesignPatternsConcepts/Address.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatternsConcepts
+{
+    internal class Address
+    {
+        public string StreetAddress { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+    }
+}
