65f1d87152915576bb3e22b4c7bab287e7320c16
allen mwita <allenmwita21@gmail.com>
2023-12-11 18:47:38 -0500

    more projects

---
+++ DesignPatternsConcepts_OG/ComputeFactory.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatternsConcepts
+{
+    internal class ComputeFactory
+    {
+        public static IComputeArea CreateObject(ComputeCriteria criteria)
+        {
+            if (criteria == ComputeCriteria.FAST)
+            {
+                return new ComputeFast();
+            }
+            else if (criteria == ComputeCriteria.ACCURATE)
+            {
+                return new ComputeAccurate();
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
---
+++ SessionTest/SessionTest/Page2.aspx.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace SessionTest
+{
+    public partial class Page2 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnReadData_Click(object sender, EventArgs e)
+        {
+            int data = (int)Session["UserData"];
+            lblUserData.Text = data.ToString();
+        }
+    }
+}
