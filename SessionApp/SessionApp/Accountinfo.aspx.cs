ab1afd36ff691602a788154006da331048a356c2
allen mwita <allenmwita21@gmail.com>
2023-12-11 20:28:07 -0500

    more projects

---
+++ SessionApp/SessionApp/Accountinfo.aspx.cs
@@ -0,0 +1,26 @@
+﻿using SessionApp.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace SessionApp
+{
+    public partial class Accountinfo : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //if (Session["LOGGEDIN"] == null)
+            if (SessionFacade.LOGGEDIN == null)
+            {
+                Response.Redirect("Login.aspx");
+            }
+            //go to db and obtain account balance
+            txtName.Text = "Bill Baker";
+            txtBalance.Text = "9878098.8987";
+            lblSessioninfo.Text = Session.SessionID;
+        }
+    }
+}
---
+++ SessionApp/SessionApp/Login.aspx.cs
@@ -0,0 +1,38 @@
+﻿using SessionApp.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace SessionApp
+{
+    public partial class Login : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnLogin_Click(object sender, EventArgs e)
+        {
+            Session.Timeout = 1; //in min
+            string uname = txtUserName.Text;
+            string password = txtPassw.Text;
+
+            //go to db to verify a valid user
+            if ((uname == "bill") && (password == "bill100")){
+                lblStatus.Text = "welcome " + uname;
+                //create session only if its a successful login
+                //Session["LOGGEDIN"] = uname;
+                SessionFacade.LOGGEDIN = uname;
+            }
+
+            else
+            {
+                lblStatus.Text = "Invalid Login";
+            }
+        }
+    }
+}
