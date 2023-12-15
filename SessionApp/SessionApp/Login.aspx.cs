67307c579ad50e4968476a0e398b740dfc2a233c
allen mwita <allenmwita21@gmail.com>
2023-12-15 08:32:57 -0500

    more projects

--- SessionApp/SessionApp/Login.aspx.cs
+++ SessionApp/SessionApp/Login.aspx.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SessionApp.Utils;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -24,7 +25,8 @@ namespace SessionApp
             if ((uname == "bill") && (password == "bill100")){
                 lblStatus.Text = "welcome " + uname;
                 //create session only if its a successful login
-                Session["LOGGEDIN"] = uname;
+                //Session["LOGGEDIN"] = uname;
+                SessionFacade.LOGGEDIN = uname;
             }
 
             else
